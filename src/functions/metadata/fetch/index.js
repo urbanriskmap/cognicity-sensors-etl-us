3c36ea330ad1acb261a8ded23d9ae00e29a1b022
ojha <ojha@MMAZ-USAID.MIT.EDU>
2017-10-06 05:36:30 -0400

    Added metadata fetch & insert handlers

--- src/functions/metadata/fetch/index.js
+++ src/functions/metadata/fetch/index.js
@@ -1,3 +1,68 @@
+const pg = require('pg');
+const dbgeoGen = require('dbgeo_gen');
+
+require('dotenv').config({silent: true});
+
+const pool = new pg.Pool({
+  user: process.env.PG_USER,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  max: process.env.PG_MAX_CLIENTS,
+  idleTimeoutMillis: process.env.PG_IDLE_TIMEOUT,
+});
+const tableName = process.env.TABLE_NAME;
+
+// TODO: Refer http://blog.rowanudell.com/database-connections-in-lambda/
+// for optimizing repetitive function invocations
 exports.getSensors = (event, context, callback) => {
-  console.log(event);
+  // Construct psql query
+  let sensorId = event.id;
+  let queryString = 'SELECT * FROM ' + tableName;
+  if (sensorId) {
+    queryString = queryString + ' WHERE id = ' + sensorId;
+  }
+
+  // Catch idle connection error
+  pool.on('error', (err, client) => {
+    console.error('Idle client error');
+    // Return error
+    callback(err);
+  });
+
+  pool.connect((err, client, done) => {
+    if (err) {
+      console.error('Database connection error');
+      // Close DB connection
+      done();
+      // Return error
+      callback(err);
+    }
+
+    let query = client.query(queryString);
+    query.on('row', (row, result) => {
+      result.addRow(row);
+    });
+    query.on('end', (result) => {
+      let geoData = result.rows;
+
+      // Convert result into topojson & return
+      console.info('Parsing sensors into topojson...');
+      dbgeoGen.parse(geoData, {
+        outputFormat: 'topojson',
+        precision: 8,
+      }, (error, output) => {
+        if (error) {
+          console.error('Data conversion failed');
+          // Close DB connection
+          done();
+          // Return error
+          callback(err);
+        }
+        console.info('Sensors data sent');
+        callback(null, output);
+      });
+    });
+  });
 };
--- src/functions/metadata/insert/index.js
+++ src/functions/metadata/insert/index.js
@@ -1,3 +1,61 @@
+const pg = require('pg');
+
+require('dotenv').config({silent: true});
+
+const pool = new pg.Pool({
+  user: process.env.PG_USER,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  max: process.env.PG_MAX_CLIENTS,
+  idleTimeoutMillis: process.env.PG_IDLE_TIMEOUT,
+});
+const tableName = process.env.TABLE_NAME;
+
+// TODO: use handler to update & delete sensors as well
 exports.addSensor = (event, context, callback) => {
-  console.log(event);
+  let sensorData;
+  if (event.body) {
+    sensorData = JSON.parse(event.body);
+    console.info('The following sensor properties will be inserted:');
+    console.info(sensorData);
+  } else {
+    console.error('No data associated with sensor');
+    callback(new Error('No data associated with sensor'));
+  }
+
+  // Catch idle connection error
+  pool.on('error', (err, client) => {
+    console.error('Idle client error');
+    // Return error
+    callback(err);
+  });
+
+  pool.connect((err, client, done) => {
+    if (err) {
+      console.error('Database connection error');
+      // Close DB connection
+      done();
+      // Return error
+      callback(err);
+    }
+
+    // Construct psql query
+    let queryString = 'INSERT INTO ' + tableName +
+      ' (location, instance_region_code, properties) ' +
+      'VALUES (ST_GeomFromText(\'POINT(' +
+      sensorData.long + ' ' + sensorData.lat + ')\'), ' +
+      sensorData.instance_region_code + ', \'' +
+      JSON.stringify(sensorData.properties) + '\');';
+
+    let query = client.query(queryString);
+    query.on('end', (result) => {
+      console.info('Sensor inserted succesfully!');
+      // Close DB connection
+      done();
+      // TODO: Construct response, return sensor id (optional: properties)
+      callback(null, result);
+    });
+  });
 };
