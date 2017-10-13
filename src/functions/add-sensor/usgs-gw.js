8dd80b262be072380c19644a16835b36d04b1c80
ojha-url <mayank.ojha@gmail.com>
2017-10-12 22:56:42 -0400

    Working addGWSensor function

--- src/functions/add-sensor/usgs-gw.js
+++ src/functions/add-sensor/usgs-gw.js
@@ -1,73 +1,111 @@
 const request = require('request');
+request.debug = true;
 
 require('dotenv').config({silent: true});
 
+let sensorCount = 0;
+const successResponse = (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else if (body.statusCode !== 200) {
+    console.log('Error');
+    console.log(body);
+  } else if (body.statusCode === 200) {
+    console.log('Sensor added');
+    sensorCount += 1;
+  }
+};
+
 exports.addGWSensor = (event, context, callback) => {
   let metadataInDB = [];
-  let newSensorMetadata;
-  let sensorCount = 0;
-  // TODO: add filter parameters in cognicity-sensors get sensors function ?
+  // TODO: add filter parameters in cognicity-sensors getSensors lambda ?
   // to filter sensors in metadata table by optional key:value pair
   // in properties (jsonb) column
-  request
-  .get('https://ckf6kf5dvd.execute-api.us-west-2.amazonaws.com/dev/sensors/')
-  .on('error', (error) => {
-    console.error(error);
-  })
-  .on('response', (response) => {
-    // store uid's from sensors metadata
-    for (let sensor of response.body.features) {
-      if (sensor.properties.properties.hasOwnProperty('uid')) {
-        metadataInDB.push(sensor.properties.properties.uid);
-      }
-    }
-    request
-    .get('https://waterservices.usgs.gov/nwis/iv/?format=json&countyCd=12011&parameterCd=62610&siteStatus=all')
-    .on('error', (error) => {
-      console.error(error);
-    })
-    .on('response', (resp) => {
-      for (let sensor of resp.value.timeSeries) {
-        let compareCount = 0;
-        for (let uid of metadataInDB) {
-          if (sensor.sourceInfo.siteCode[0].value === uid) {
-            compareCount += 1;
-            break;
-          }
+  console.log('Sending get request to sensors lambda');
+  request.get('https://ckf6kf5dvd.execute-api.us-west-2.amazonaws.com/dev/sensors/',
+  (error, response, body1) => {
+    if (error) {
+      return console.error(error);
+    } else {
+      console.log('Received data from sensors/metadata');
+      let geojson = JSON.parse(body1).body;
+      // store uid's from sensors metadata
+      for (let feature of geojson.features) {
+        if (feature.properties.properties.hasOwnProperty('uid')) {
+          metadataInDB.push(feature.properties.properties.uid);
         }
-        if (compareCount === 0) {
-          let sensorType;
-          let uid = sensor.sourceInfo.siteCode[0].value;
-          for (let property of sensor.sourceInfo.siteProperty) {
-            if (property.name === 'siteTypeCd') {
-              sensorType = property.value;
+      }
+
+      // Get sensors metadata from USGS source
+      request.get('https://waterservices.usgs.gov/nwis/iv/?format=json&countyCd=12011&parameterCd=62610&siteStatus=all',
+      (err, resp, body2) => {
+        if (err) {
+          return console.error(err);
+        } else {
+          console.log('Received data from USGS GW query');
+          let sensors = JSON.parse(body2).value;
+          for (let sensor of sensors.timeSeries) {
+            let sensorExists = false;
+
+            // Check if sensor unique identifier exists in metadata table
+            for (let uid of metadataInDB) {
+              if (sensor.sourceInfo.siteCode[0].value === uid) {
+                sensorExists = true;
+                break;
+              }
+            }
+
+            // Extract sensor properties & POST to /sensors
+            if (!sensorExists) {
+              let uid = sensor.sourceInfo.siteCode[0].value;
+              let sensorType;
+              for (let property of sensor.sourceInfo.siteProperty) {
+                if (property.name === 'siteTypeCd') {
+                  sensorType = property.value;
+                }
+              }
+
+              // Construct body for request
+              let newSensorMetadata = {
+                properties: {
+                  uid: uid,
+                  type: sensorType,
+                  query_parameters: {
+                    cb_62610: 'on',
+                    format: 'rdb',
+                    site_no: uid,
+                    period: 2,
+                  },
+                },
+                location: {
+                  lat: sensor.sourceInfo.geoLocation.geogLocation.latitude,
+                  lng: sensor.sourceInfo.geoLocation.geogLocation.longitude,
+                },
+              };
+
+              let postRequestOptions = {
+                url: 'https://ckf6kf5dvd.execute-api.us-west-2.amazonaws.com/dev/sensors/',
+                headers: {
+                  'Content-Type': 'application/json',
+                  'x-api-key': process.env.API_KEY,
+                },
+                json: newSensorMetadata,
+                // Sets body object to json,
+                // assigning {object} to body is not allowed,
+                // only string, array or buffer are supported
+              };
+
+              request.post(postRequestOptions, successResponse);
             }
           }
-          newSensorMetadata = {
-            uid: uid,
-            type: sensorType,
-            geoLocation: sensor.sourceInfo.geoLocation.geogLocation,
-            query_parameters: {
-              cb_62610: 'on',
-              format: 'rdb',
-              site_no: uid,
-              period: 2,
-            },
-          };
-          request
-          .post({
-            url: 'https://ckf6kf5dvd.execute-api.us-west-2.amazonaws.com/dev/sensors/',
-            headers: {
-              'Content-Type': 'application/json',
-              'x-api-key': process.env.API_KEY,
-            },
-          }, newSensorMetadata);
-          sensorCount += 1;
+
+          // Send status
+          let msg = sensorCount +
+          ((sensorCount === 0 || sensorCount === 1) ? ' sensor' : ' sensors') +
+          ' added';
+          callback(null, msg);
         }
-      }
-      console.log(sensorCount +
-      ((sensorCount === 0 || sensorCount === 1) ? ' sensor' : ' sensors') +
-      ' added');
-    });
+      });
+    }
   });
 };
