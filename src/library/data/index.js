6a99134cf518c3939d2803e3bc7e9921f770de7f
ojha-url <mayank.ojha@gmail.com>
2018-08-08 11:02:10 -0400

    Fixed datetime formatting for sfwmd api

--- src/library/data/index.js
+++ src/library/data/index.js
@@ -5,6 +5,17 @@ export default class {
   }
 
   getWmdQueryTimeFormat() {
+    const formatDateString = (date) => {
+      return date.getFullYear()
+      + '-' + // getMonth returns integer between 0 & 11, required 01 & 12
+      (date.getMonth() < 9 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1)
+      + '-' + // getDate returns integer between 1 & 31, required 01 & 31
+      (date.getDate() < 10 ? '0' + date.getDate() : date.getDate())
+      + // getHours returns integer between 0 & 23, required 00 & 23
+      (date.getHours() < 10 ? '0' + date.getHours() : date.getHours())
+      + ':00:00:000';
+    };
+
     const periodMilliseconds = parseInt(
       this.config.RECORDS_PERIOD.slice(1, -1),
       10
@@ -13,11 +24,8 @@ export default class {
     const now = new Date();
     const start = new Date(Date.parse(now) - periodMilliseconds);
 
-    const begin = start.getFullYear() + '-' + start.getMonth() + '-'
-    + start.getDate() + start.getHours() + ':' + start.getMinutes() + ':00:000';
-
-    const end = now.getFullYear() + '-' + now.getMonth() + '-' + now.getDate()
-    + now.getHours() + ':' + now.getMinutes() + ':00:000';
+    const begin = formatDateString(start);
+    const end = formatDateString(now);
 
     return {
       begin: begin,
