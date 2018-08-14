2fe4f40537c39237d9731a715060b9b6f0cace29
ojha-url <mayank.ojha@gmail.com>
2018-08-14 16:06:16 -0400

    Added compare sensors service

---
+++ src/services/sensor/compare.js
@@ -0,0 +1,38 @@
+/**
+ * This method compares a sensor against a list of
+ * all stored sensors using the unique id value
+ * @function compareSensors
+ * @param {object} sensor - Sensor interface
+ * @param {string} uniqueIdKey - Sensor unique id property
+ * @param {string[]} existingSensorUids - list of sensor uid's
+ * @return {Promise} Promise object
+ */
+export default (sensor, uniqueIdKey, existingSensorUids) => {
+  return new Promise(function(resolve, reject) {
+    if (sensor.hasOwnProperty('log')) {
+      resolve(sensor);
+    } else {
+      let sensorExists = false;
+      const sensorUid = sensor[uniqueIdKey];
+
+      if (existingSensorUids.length) {
+        for (const uidExisting of existingSensorUids) {
+          if (sensorUid === uidExisting) {
+            sensorExists = true;
+            break;
+          }
+        }
+
+        if (!sensorExists) {
+          resolve(sensor);
+        } else {
+          resolve({
+            log: sensorUid + ': Sensor already exists',
+          });
+        }
+      } else {
+        resolve(sensor);
+      }
+    }
+  });
+};
