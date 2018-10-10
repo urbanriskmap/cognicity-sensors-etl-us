4a27b0ab025a9da5db6960cb3fddb9357a0e5e3f
ojha-url <mayank.ojha@gmail.com>
2018-10-10 16:37:16 -0400

    Added NOAA stations for panhandle

--- src/functions/sensors/noaa/stations.js
+++ src/functions/sensors/noaa/stations.js
@@ -15,5 +15,76 @@ export default {
         lng: -80.1167,
       },
     },
+    // Panhandle stations
+    {
+      properties: {
+        agency: 'noaa',
+        datum: 'MLLW',
+        name: 'Pensacola',
+        station: 8729840,
+        time_zone: 'LST_LDT',
+        units: 'ft',
+      },
+      location: {
+        lat: 30.4044,
+        lng: -87.2112,
+      },
+    },
+    {
+      properties: {
+        agency: 'noaa',
+        datum: 'MLLW',
+        name: 'Panama City Beach',
+        station: 8729210,
+        time_zone: 'LST_LDT',
+        units: 'ft',
+      },
+      location: {
+        lat: 30.2133,
+        lng: -85.8783,
+      },
+    },
+    {
+      properties: {
+        agency: 'noaa',
+        datum: 'MLLW',
+        name: 'Panama City',
+        station: 8729108,
+        time_zone: 'LST_LDT',
+        units: 'ft',
+      },
+      location: {
+        lat: 30.1523,
+        lng: -85.6669,
+      },
+    },
+    {
+      properties: {
+        agency: 'noaa',
+        datum: 'MLLW',
+        name: 'Apalachicola',
+        station: 8728690,
+        time_zone: 'LST_LDT',
+        units: 'ft',
+      },
+      location: {
+        lat: 29.7267,
+        lng: -84.9817,
+      },
+    },
+    {
+      properties: {
+        agency: 'noaa',
+        datum: 'MLLW',
+        name: 'Cedar Key',
+        station: 8727520,
+        time_zone: 'LST_LDT',
+        units: 'ft',
+      },
+      location: {
+        lat: 29.1336,
+        lng: -83.0309,
+      },
+    },
   ],
 };
